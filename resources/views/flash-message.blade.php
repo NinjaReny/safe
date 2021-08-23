0214d3a149db8a266a57bd20f8e3c40a58de807f
Chrishanth Niranjan Reny <chrishanth.niranjan@gmail.com>
2021-08-23 20:01:14 +0530

    restored comment

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -32,13 +32,6 @@
         Please check the form below for errors
     </div>
 @endif
-<!-- 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        Please check the form below for errors
-    </div>
-@endif -->
 
 <script>
     $("document").ready(function(){
